4ed50b7e821b372a7af6f9daab785d3a57d8393d
Canberk Demirsoy <canberk.demirsoy@tum.de>
2019-01-16 16:02:10 +0100

    hack better4

--- modules/control/control_component.cc
+++ modules/control/control_component.cc
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *****************************************************************************/
+hhhklkh
 #include "modules/control/control_component.h"
 
 #include "cyber/common/log.h"
